a702d8b65de08026f99024df5b4498acc8536924
Parth Bhargava <parth.bhargava2004@gmail.com>
2026-03-04 01:21:23 +0530

    app.py update

--- app.py
+++ app.py
@@ -207,6 +207,10 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+# Render is a real server, so we can do this normally!
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
